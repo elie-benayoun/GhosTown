0e8224972699f2b6c6a4c4fe57b80e24be3365d8
IPan21 <ipan2109@gmail.com>
2020-06-10 13:03:35 +0300

    style fixes

--- src/components/success.jsx
+++ src/components/success.jsx
@@ -4,11 +4,14 @@ import Grid from '@material-ui/core/Grid';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 import { green } from '@material-ui/core/colors';
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
     root: {
       minWidth: 275,
+      height: '100%',
+      marginLeft: '40px'
     },
     bullet: {
       display: 'inline-block',
@@ -47,10 +50,11 @@ export function Success(props) {
 
 
 export function Fail(props) {
+    const classes = useStyles()
     return (
         <div>
             <Paper variant="outlined">
-            <Grid container alignItems='center' justify='center'>
+            <Grid container alignItems='center' justify='center'  className={classes.root}>
                 <Grid xs={12}>
                     <div style={{textAlign: 'center'}}>
                     <div>According to Our prediction  your AirBnb is in a bad situation</div>
@@ -68,15 +72,34 @@ export function Fail(props) {
 
 
 export function Default(props) {
+    const classes = useStyles()
     return (
         <div>
-            <Paper variant="outlined" >
-            <Grid container alignItems='center' justify='center'>
+            <Paper variant="outlined" className={classes.root}>
+            <Grid container >
                 <Grid xs={12}>
-                    <div style={{textAlign: 'center'}}>
-                    <div>According to Our prediction  your AirBnb is in a bad situation</div>
-                    <ThumbDownIcon style={{ fontSize: 150, color: "#f50057" }}/>
-                    <div>Your chances to succeed are : {(props.data[0][1]*100).toString()} %</div>
+                    <div style={{textAlign: 'left', display: "flex", padding: '20px', marginTop: '90px'}}>
+                    <div style={{textAlign: 'left', display: "inline-block", alignSelf: "flex-end", padding: '20px'}}>
+                        <Typography gutterBottom variant="h6" component="h3" color="textSecondary" >Enter the details about your AirBNB in the form on the left. We will help you decide whether you should keep operating.
+
+                            What we need from you: 
+                                <ul>
+                                    <li>Your address</li>
+                                    <li>The minimum number of nights renters need to commit to</li>
+                                    <li>How many days a year will the asset be available </li>
+                                    <li>Price per night </li>
+                                    <li>New York Borough </li>
+                                    <li>Your asset type</li>
+
+                                </ul>
+                                </Typography>
+                                <div style={{textAlign: 'center'}}>
+                                <Typography gutterBottom variant="h6" component="h3" color="textSecondary" > Good luck!
+                                </Typography>
+                                </div></div>
+
+            
+                
                     </div>
                     </Grid>
                     
